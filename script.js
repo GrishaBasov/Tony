64b0c1c43be4cdea2c9ad41616cda1a3d8520481
g.basov <g.basov@okko.tv>
2025-07-22 17:09:51 +0300

    Add all poems, add style, add logic

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const poemElement = document.getElementById('poem');
     const randomPoem = poems[Math.floor(Math.random() * poems.length)];
-    poemElement.innerHTML = randomPoem;
+
+
+    poemElement.innerHTML = randomPoem.split('\n').map(line => line.trim() === ''
+            ? '<div class="empty-line"></div>'
+            : `<span class="poem-line">${line}</span><br>`
+    ).join('');
 });
